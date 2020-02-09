fbfb36c45989fbfba990d678a76a37ec23564f1e
Ageroge <60846655+Ageroge@users.noreply.github.com>
2020-02-09 21:58:14 +0200

    Add files via upload
    tuned parameters. But still not final, as I see odd behavior of agent

--- SwordsmanScript1.cs
+++ SwordsmanScript1.cs
@@ -5,62 +5,72 @@ using MLAgents;
 
 public class SwordsmanScript1 : Agent
 {
+    // hit points of swordsman. if 0, then he is dead
+    public int hitpoints_g = 2;
+    // speed of agent. Valu is overridden in Unity interface.
+    public float speed = 12;
+
     Rigidbody rBody;
+
+    // Is agent collided with arrow?
     private bool is_collided = false;
     // Size of arena. Need to pass it automatically somehow?
     int arenaSizeX = 16;
     int arenaSizeY = 16;
-    float prevDistanceToTarget;
+    // minimal distance between agent and target
+    float minDistanceToTarget;
+    int hitpoints = 2;
 
     void Start()
     {
         rBody = GetComponent<Rigidbody>();
     }
 
-    public Transform Target;
+    public Transform Target1;
+    public Transform Target2;
+    // HOW to ADD another archer???
+
     public override void AgentReset()
     {
         // Move the target to a new spot
         float randomVal1 = Random.value;
         float randomVal2 = Random.value;
         float randomVal3 = Random.value;
-        if (randomVal3 <= 0.5f)
-        {
+        if (randomVal3 <= 0.5f) {
             if (randomVal1 <= 0.5f) randomVal1 = 0.0f; else randomVal1 = 1.0f;
         }
-        else
-        {
+        else {
             if (randomVal2 <= 0.5f) randomVal2 = 0.0f; else randomVal2 = 1.0f;
         }
 
-        Target.localPosition = new Vector3(randomVal1 * arenaSizeX - arenaSizeX / 2, 0.5f, randomVal2 * arenaSizeY - arenaSizeY / 2);
+        Target1.localPosition = new Vector3(randomVal1 * arenaSizeX - arenaSizeX / 2, 0.5f, randomVal2 * arenaSizeY - arenaSizeY / 2);
 
-        if (this.transform.localPosition.y < 0 || is_collided)
-        {
-            // If the Agent fell, zero its momentum
-            this.rBody.angularVelocity = Vector3.zero;
-            this.rBody.velocity = Vector3.zero;
-            // make opposite position to archer
-            this.transform.localPosition = new Vector3(-(randomVal1 * arenaSizeX - arenaSizeX / 2), 0.5f, -(randomVal2 * arenaSizeY - arenaSizeY / 2));
-            is_collided = false;
-        }
+        // zero momentum of agent
+        this.rBody.angularVelocity = Vector3.zero;
+        this.rBody.velocity = Vector3.zero;
+        // set opposite position to archer (target)
+        this.transform.localPosition = new Vector3(-(randomVal1 * arenaSizeX - arenaSizeX / 2), 0.5f, -(randomVal2 * arenaSizeY - arenaSizeY / 2));
+        hitpoints = hitpoints_g;
+        is_collided = false;
 
-        prevDistanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
-//        Debug.Log(initDistanceToTarget);
+        // initialize minimal distance of agent to target. Will be used to rewards further
+        minDistanceToTarget = Vector3.Distance(this.transform.localPosition, Target1.localPosition);
     }
 
     public override void CollectObservations()
     {
         // Target and Agent positions
-        AddVectorObs(Target.localPosition);
+        AddVectorObs(Target1.localPosition);
         AddVectorObs(this.transform.localPosition);
 
         // Agent velocity
         AddVectorObs(rBody.velocity.x);
         AddVectorObs(rBody.velocity.z);
+
+        // Agent hitpoints
+        AddVectorObs(this.hitpoints);
     }
 
-    public float speed = 12;
     public override void AgentAction(float[] vectorAction)
     {
         // Actions, size = 2
@@ -69,33 +79,37 @@ public class SwordsmanScript1 : Agent
         controlSignal.z = vectorAction[1];
         rBody.AddForce(controlSignal * speed);
 
-        // Rewards
         float distanceToTarget = Vector3.Distance(this.transform.localPosition,
-                                                  Target.localPosition);
+                                                  Target1.localPosition);
 
-        if (prevDistanceToTarget > distanceToTarget) AddReward(0.01f);
-        else AddReward(-0.01f);
-
-        prevDistanceToTarget = distanceToTarget;
+        // if agents is closer to the target, give him small award
+        // and penatly otherwise
+        if (distanceToTarget < minDistanceToTarget) {
+            AddReward(0.001f);
+            minDistanceToTarget = distanceToTarget;
+        }
+        else AddReward(-0.001f);
 
         // Reached target
-        if (distanceToTarget < 1.42f)
-        {
+        if (distanceToTarget < 1.42f) {
             AddReward(1.0f);
             Done();
         }
 
         // Fell off platform
-        if (this.transform.localPosition.y < 0)
-        {
+        if (this.transform.localPosition.y < 0) {
             AddReward(-1.0f);
             Done();
         }
-        // Killed by arrow
-        if (is_collided)
-        {
-            AddReward(-0.5f);
-            Done();
+        // Hit by arrow and maybe even killed
+        if (is_collided) {
+            AddReward(-0.2f);
+            hitpoints--;
+            is_collided = false;
+            if (hitpoints == 0) {
+                AddReward(-0.6f);
+                Done();
+            }
         }
 
     }
