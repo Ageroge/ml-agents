1634b7f8f3e2731402719f61e8a83b4da4ee326d
Ageroge <60846655+Ageroge@users.noreply.github.com>
2020-02-09 15:57:44 +0200

    Add files via upload

---
+++ ArcherScript.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class ArcherScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    // public Rigidbody projectile;
+    public GameObject arrow;
+    public float arrow_speed = 20;
+    // speed of rotating archer
+    public float angle_speed = 2.0f;
+    // timer to make delays between shots
+    public float targetTime = 2.5f;
+
+    public GameObject swordsman;
+//    private GameObject hunter;
+
+    void Start()
+    {
+//        hunter = GameObject.Find("Hunter_agent");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtSwordsman(swordsman.transform);
+
+        targetTime -= Time.deltaTime;
+//        if (Input.GetButtonDown("Fire1"))
+        if (targetTime <= 0.0f)
+        {
+            targetTime = 2.5f;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject instArrow = Instantiate(arrow, transform.position, Quaternion.identity) as GameObject;
+        Rigidbody instArrowRigidbody = instArrow.GetComponent<Rigidbody>();
+
+//        Debug.Log("this rot=" + transform.rotation);
+
+        instArrowRigidbody.velocity = transform.TransformDirection(Vector3.forward * arrow_speed);
+        
+        Destroy(instArrow, 2.0f); // 1 - time in seconds. Live range of arrow
+    }
+
+    void LookAtSwordsman(Transform target)
+    {
+        // Determine which direction to rotate towards
+        Vector3 targetDirection = target.position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = angle_speed * Time.deltaTime;
+
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+        // Draw a ray pointing at our target in
+        Debug.DrawRay(transform.position, newDirection, Color.red);
+
+        // Calculate a rotation a step closer to the target and applies rotation to this object
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+
+
+    /*    void Shoot()
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, hunter.transform.position, out hit, range))
+            {
+                Debug.Log(hit.transform.name);
+            }
+            else
+            {
+                Debug.Log("no hit");
+            }
+        }
+    */
+}
---
+++ SwordsmanScript1.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class SwordsmanScript1 : Agent
+{
+    Rigidbody rBody;
+    private bool is_collided = false;
+    // Size of arena. Need to pass it automatically somehow?
+    int arenaSizeX = 16;
+    int arenaSizeY = 16;
+    float prevDistanceToTarget;
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    public Transform Target;
+    public override void AgentReset()
+    {
+        // Move the target to a new spot
+        float randomVal1 = Random.value;
+        float randomVal2 = Random.value;
+        float randomVal3 = Random.value;
+        if (randomVal3 <= 0.5f)
+        {
+            if (randomVal1 <= 0.5f) randomVal1 = 0.0f; else randomVal1 = 1.0f;
+        }
+        else
+        {
+            if (randomVal2 <= 0.5f) randomVal2 = 0.0f; else randomVal2 = 1.0f;
+        }
+
+        Target.localPosition = new Vector3(randomVal1 * arenaSizeX - arenaSizeX / 2, 0.5f, randomVal2 * arenaSizeY - arenaSizeY / 2);
+
+        if (this.transform.localPosition.y < 0 || is_collided)
+        {
+            // If the Agent fell, zero its momentum
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            // make opposite position to archer
+            this.transform.localPosition = new Vector3(-(randomVal1 * arenaSizeX - arenaSizeX / 2), 0.5f, -(randomVal2 * arenaSizeY - arenaSizeY / 2));
+            is_collided = false;
+        }
+
+        prevDistanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+//        Debug.Log(initDistanceToTarget);
+    }
+
+    public override void CollectObservations()
+    {
+        // Target and Agent positions
+        AddVectorObs(Target.localPosition);
+        AddVectorObs(this.transform.localPosition);
+
+        // Agent velocity
+        AddVectorObs(rBody.velocity.x);
+        AddVectorObs(rBody.velocity.z);
+    }
+
+    public float speed = 12;
+    public override void AgentAction(float[] vectorAction)
+    {
+        // Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.z = vectorAction[1];
+        rBody.AddForce(controlSignal * speed);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition,
+                                                  Target.localPosition);
+
+        if (prevDistanceToTarget > distanceToTarget) AddReward(0.01f);
+        else AddReward(-0.01f);
+
+        prevDistanceToTarget = distanceToTarget;
+
+        // Reached target
+        if (distanceToTarget < 1.42f)
+        {
+            AddReward(1.0f);
+            Done();
+        }
+
+        // Fell off platform
+        if (this.transform.localPosition.y < 0)
+        {
+            AddReward(-1.0f);
+            Done();
+        }
+        // Killed by arrow
+        if (is_collided)
+        {
+            AddReward(-0.5f);
+            Done();
+        }
+
+    }
+
+    public override float[] Heuristic()
+    {
+        var action = new float[2];
+        action[0] = Input.GetAxis("Horizontal");
+        action[1] = Input.GetAxis("Vertical");
+        return action;
+    }
+
+    void OnCollisionEnter(Collision collided)
+    {
+        if (collided.gameObject.tag == "arrow")
+        {
+            is_collided = true;
+        }
+
+    }
+}
